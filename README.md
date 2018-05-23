7ca633078d7e92acc5bc139dd1696f7c344687ad
Jabrils <ankelzspam@gmail.com>
2018-05-22 23:39:58 -0700

    added removal keyword function
    :+1:

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ lowercase AND remove Duplicates - A Very Simple Program;
 
 I find myself having to clean up data quite often & lose python scripts that I write for data cleaning. So I've decided to start this lcNrD project that is a but more of a permanent solution & I will add to its usefulness as I see fit.
 
+## Disclaimer:
+As it stands, this program works best with text listed using the '\n' delimiter. I plan to soon add more delimiter options
+
 ## Current Functionality:
 * Lowercase the text of a file
 * Remove duplicates of a file
 * Copy all operations onto a new file
+* Remove lines containing a keyword
 
 ## Program Arguments: (* = required)
 
@@ -19,3 +23,8 @@ I find myself having to clean up data quite often & lose python scripts that I w
   -l, --lowercase       add -l to set lowercase to true
   -d, --duplicates      add -d to remove duplicate elements
 ```
+
+## Roadmap:
+* Add input delimiter
+* Add output delimiter
+* Add function that removes all lines except ones that contain a keyword
--- lcNrD.py
+++ lcNrD.py
@@ -1,11 +1,11 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument(
     "-f", "--file", type=str,
     help='this is the file you\'re targeting to get the lcNrD treatment')
 parser.add_argument("-o", "--out", type=str,
-    help='this is the file you want the lcNrD file to output as')
+    help='this is the file you want the lcNrD file to output as. Default = same name + _LcNrD')
 
 # store true or false so we can just be like 'if args.lowercase:'
 # so use true or false values instead of comparing strings
@@ -13,26 +13,46 @@ parser.add_argument('-l', "--lowercase", action='store_true',
     help='add -l to set lowercase to true')
 parser.add_argument('-d', "--duplicates", action='store_true',
     help='add -d to remove duplicate elements')
+parser.add_argument('-rk', "--removal_keyword", type=str,
+    help='add -rk to remove lines with the removal keyword')
 args = parser.parse_args()
 
+print(args.removal_keyword)
+
 print('Grabbing file contents...')
+
 # open the target file & grab its contents. Using `with` like this automatically
 # closes the file for us when we leave the indentation.
 with open(args.file, "r") as fp:
     text = fp.read()
 
+# lets turn every line into an array 
+lines = text.split("\n")
+
+# this checks if there is a removal keyword & performs this operation if so
+if args.removal_keyword != None:
+    print('removing lines with keyword...')
+    temp = ''
+
+    # loop through all elements in the lines array
+    for i in lines:
+        # if not containing the removal keyword, then add it to temp
+        if args.removal_keyword not in i:
+            temp+='\n'+i
+    
+    # lines now = temp split, minus the first element which is an empty element
+    lines = temp.split('\n')[1:]
+
 # convert to lowercase while we have the initial string. so only one function
 # call and no loops needed.
 if args.lowercase:
     print('lowercasing contents...')
-    text = text.lower()
+    lines = [x.lower() for x in lines]
 
-# need to store 
-lines = text.split("\n")
 
 # remove duplicates by making the list a `set` which automatically makes
 # everything unique. Again no loops, just 1 function call.
-if args.unique:
+if args.duplicates:
     print('removing duplicates...')
     lines = set(lines)
 
@@ -42,6 +62,7 @@ if not args.out:
     # a list once so 'dir/file.txt' -> ['dir/file', 'txt']. then the '*' before
     # it when passed to format tells format to read the list as mulptiple args.
     args.out = "{}._lcNrD.{}".format(*args.file.rsplit(".", 1))
+
 
 # save the converted file
 with open(args.out, "w") as fp:
